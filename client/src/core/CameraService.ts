c89a31a072473b4b61b4059f82bd0f61d02a64d6
grakeice <89824152+grakeice@users.noreply.github.com>
2025-07-19 03:07:01 +0900

    CameraService.tsにJSDocを付けたよ

--- client/src/core/CameraService.ts
+++ client/src/core/CameraService.ts
@@ -5,15 +5,27 @@
  * https://opensource.org/licenses/MIT
  */
 
+/**
+ * ブラウザ上でカメラを扱うためのサービスクラス
+ */
 export class CameraService {
 	private static instance: CameraService;
 	private static cameraStream: MediaStream;
 
+	/**
+	 * CameraServiceのインスタンスを取得
+	 * @returns CameraServiceのインスタンス
+	 */
 	static getInstance() {
 		CameraService.instance ??= new CameraService();
 		return CameraService.instance;
 	}
 
+	/**
+	 * Webカメラの初期設定をする
+	 * @param settings Webカメラ使用時の設定
+	 * @returns MediaStreamオブジェクト
+	 */
 	async start(settings: MediaStreamConstraints) {
 		try {
 			CameraService.cameraStream =
@@ -24,6 +36,10 @@ export class CameraService {
 		}
 	}
 
+	/**
+	 * Webカメラの映像を描画する
+	 * @param target Webカメラの映像の描画先
+	 */
 	render(target: HTMLVideoElement) {
 		target.srcObject = CameraService.cameraStream;
 	}
