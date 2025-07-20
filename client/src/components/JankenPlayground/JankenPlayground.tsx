8f31d3f16bab92e27459f88cfd823a4a433d0aad
grakeice <89824152+grakeice@users.noreply.github.com>
2025-07-21 04:22:47 +0900

    簡単にじゃんけんできるようにはなった

--- client/src/components/JankenPlayground/JankenPlayground.tsx
+++ client/src/components/JankenPlayground/JankenPlayground.tsx
@@ -6,7 +6,8 @@
  */
 
 import { type JSX, useRef, useState } from "react";
-import { JankenHand } from "../../core";
+import { useDebounce } from "react-use";
+import { Janken, JankenHand } from "../../core";
 import { KaoJanken, type KaoJankenUserComponent } from "../KaoJanken";
 
 export function JankenPlayground(): JSX.Element {
@@ -16,11 +17,32 @@ export function JankenPlayground(): JSX.Element {
 	const [currentUserGesture, setCurrentUserGesture] = useState<JankenHand>(
 		JankenHand.UNKNOWN,
 	);
+	const [currentComputerGesture, setCurrentComputerGesture] =
+		useState<JankenHand>(JankenHand.UNKNOWN);
 
 	const handleStartButtonClick = async () => {
 		await kaoJankenUserRef.current?.start();
 		setPlaying(true);
 	};
+
+	useDebounce(
+		() => {
+			if(currentUserGesture === JankenHand.UNKNOWN){
+				setCurrentComputerGesture(JankenHand.UNKNOWN);
+				return;
+			}
+			//0から3までのランダムな整数を生成する
+			const randomGesture = [JankenHand.GU, JankenHand.CHOKI, JankenHand.PA][
+				Math.floor(Math.random() * 2)
+			];
+			setCurrentComputerGesture(randomGesture);
+			const user = new Janken(currentUserGesture);
+			const computer = new Janken(currentComputerGesture);
+			console.log(user.examine(computer));
+		},
+		3000,
+		[currentUserGesture, currentComputerGesture],
+	);
 
 	return (
 		<div className="flex size-4 h-fit w-fit flex-col items-center gap-4 rounded-4xl border-6 border-base-300 p-10 text-center">
@@ -32,7 +54,10 @@ export function JankenPlayground(): JSX.Element {
 					setCurrentGesture={setCurrentUserGesture}
 				/>
 				<h1>vs</h1>
-				<KaoJanken.Computer />
+				<KaoJanken.Computer
+					currentGesture={currentComputerGesture}
+					setCurrentGesture={setCurrentComputerGesture}
+				/>
 			</div>
 			<p>
 				<button
--- client/src/components/KaoJanken/Computer/Computer.tsx
+++ client/src/components/KaoJanken/Computer/Computer.tsx
@@ -5,14 +5,53 @@
  * https://opensource.org/licenses/MIT
  */
 
-import type { JSX } from "react";
+import type { Dispatch, JSX, SetStateAction } from "react";
+import { JankenHand } from "../../../core";
+import chokiImage from "../assets/choki_background.svg";
+import guImage from "../assets/gu_background.svg";
+import paImage from "../assets/pa_background.svg";
 import styles from "./Computer.module.css";
 
-export function Computer(): JSX.Element {
+interface ComputerProps {
+	currentGesture: JankenHand;
+	setCurrentGesture: Dispatch<SetStateAction<JankenHand>>;
+}
+export function Computer({
+	currentGesture
+}: ComputerProps): JSX.Element {
+	const renderComputerHandImage = () => {
+		switch (currentGesture) {
+			case JankenHand.GU:
+				return (
+					<img className={styles["hand-image"]} src={guImage} alt="グー" />
+				);
+			case JankenHand.CHOKI:
+				return (
+					<img className={styles["hand-image"]} src={chokiImage} alt="チョキ" />
+				);
+			case JankenHand.PA:
+				return (
+					<img className={styles["hand-image"]} src={paImage} alt="パー" />
+				);
+			default:
+				return;
+		}
+	};
 	return (
-		<div>
+		<div className="flex flex-col items-center justify-between">
 			<h1>computer</h1>
-			<div className={styles.hand}></div>
+			<div
+				style={{
+					width: "20rem",
+					height: "20rem",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				{renderComputerHandImage()}
+			</div>
+			<button type="button" style={{ visibility: "hidden" }}></button>
 		</div>
 	);
 }
