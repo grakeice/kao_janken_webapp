ead71b984e96a189620eccd79c6e2037d81f9d25
grakeice <89824152+grakeice@users.noreply.github.com>
2025-07-29 16:24:04 +0900

    勝ち負けの数が表示されるように

--- client/src/components/JankenPlayground/JankenPlayground.tsx
+++ client/src/components/JankenPlayground/JankenPlayground.tsx
@@ -12,10 +12,19 @@ import { sleep } from "@/utils";
 import { KaoJanken, type KaoJankenUserComponent } from "../KaoJanken";
 import type { KaoJankenComputerComponent } from "../KaoJanken/Computer";
 import { type KaoJankenModalComponent, Modal } from "../Modal";
+import { Record } from "../Record";
 
 export function JankenPlayground(): JSX.Element {
 	const [playingFlag, setPlayingFlag] = useState(false);
 	const afkFlag = useRef(false);
+
+	const [winCount, setWinCount] = useState(0);
+	const [loseCount, setLoseCount] = useState(0);
+	const [drawCount, setDrawCount] = useState(0);
+
+	const incrementWinCount = () => setWinCount(winCount + 1);
+	const incrementLoseCount = () => setLoseCount(loseCount + 1);
+	const incrementDrawCount = () => setDrawCount(drawCount + 1);
 
 	const kaoJankenUserRef = useRef<KaoJankenUserComponent>(null);
 	const kaoJankenComputerRef = useRef<KaoJankenComputerComponent>(null);
@@ -43,7 +52,20 @@ export function JankenPlayground(): JSX.Element {
 			if (!playingFlag) return;
 			const user = new Janken(currentUserGesture);
 			const computer = new Janken(currentComputerGesture);
-			kaoJankenModalRef.current?.show(user.examine(computer));
+			const result = user.examine(computer);
+			kaoJankenModalRef.current?.show(result);
+			switch (result) {
+				case "win":
+					incrementWinCount();
+					break;
+				case "lose":
+					incrementLoseCount();
+					break;
+				case "draw":
+					incrementDrawCount();
+					break;
+				default:
+			}
 			kaoJankenUserRef.current?.pause();
 			await sleep(2000);
 			kaoJankenComputerRef.current?.stop();
@@ -70,6 +92,7 @@ export function JankenPlayground(): JSX.Element {
 		<>
 			<Modal ref={kaoJankenModalRef} />
 			<div className="flex size-4 h-fit w-fit flex-col items-center gap-4 rounded-box border-6 border-base-300 p-10 text-center">
+				<Record wins={winCount} loses={loseCount} draws={drawCount} />
 				<h1>KaoJanken</h1>
 				<div className="container flex flex-row justify-around gap-4">
 					<KaoJanken.User
---
+++ client/src/components/Record/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2025 grakeice
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import { Record } from "./Record";
+
+export { Record };
