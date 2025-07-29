1b95671f54e77674d9df5cce7fb3d097d1341188
grakeice <89824152+grakeice@users.noreply.github.com>
2025-07-29 20:20:56 +0900

    getRandomIntがMDNからの引用であることを明記

--- client/src/utils/random.ts
+++ client/src/utils/random.ts
@@ -1,5 +1,8 @@
+/**
+ *  MDNからの引用: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ */
 export function getRandomInt(min: number, max: number) {
 	const minCeiled = Math.ceil(min);
 	const maxFloored = Math.floor(max);
-	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // 上限は除き、下限は含む
+	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
